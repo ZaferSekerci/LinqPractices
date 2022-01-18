f42b997d0708837f3684c9405a93c096e645c5e3
Zafer Şekerci <zafersekerci@ya.ru>
2022-01-18 23:12:35 +0300

    most used linq methods tested

--- DBOperations/DataGenerator.cs
+++ DBOperations/DataGenerator.cs
@@ -13,28 +13,24 @@ namespace LinqPractices.DBOperations
                 context.AddRange(
                     new Student()
                     {
-                        StudentID = 1,
                         Name = "Zafer",
                         Surname = "Yılmaz",
                         ClassID = 2
                     },
                     new Student()
                     {
-                        StudentID = 2,
                         Name = "Dilek",
                         Surname = "Palkara",
                         ClassID = 3
                     },
                     new Student()
                     {
-                        StudentID = 3,
                         Name = "Ayşe",
                         Surname = "Uygun",
                         ClassID = 3
                     },
                     new Student()
                     {
-                        StudentID = 4,
                         Name = "Ali",
                         Surname = "Algın",
                         ClassID = 3
--- Program.cs
+++ Program.cs
@@ -11,8 +11,47 @@ namespace LinqPractices
             DataGenerator.Initialize();
             LinqDbContext _context = new LinqDbContext();
             var students = _context.Students.ToList<Student>();
+            //find
+            Console.WriteLine("******find******");
+            var student = _context.Students.Where(student => student.StudentID == 1).FirstOrDefault();
+            student = _context.Students.Find(2);
+            Console.WriteLine(student.Name);
+
+            //first or default
+            Console.WriteLine("******find\n");
+            student = _context.Students.Where(student => student.Surname == "Yılmaz").FirstOrDefault();
+            Console.WriteLine(student.Name);
+            student = _context.Students.FirstOrDefault(x => x.Surname == "Yılmaz");
+            Console.WriteLine(student.Name);
 
 
+            //single or default
+            Console.WriteLine("******single or def\n");
+            student = _context.Students.SingleOrDefault(student => student.Name == "Zafer");
+            Console.WriteLine(student.Name);
+
+            //tolist
+            Console.WriteLine("******tolist\n");
+            var studentList = _context.Students.Where(student => student.ClassID == 3).ToList();
+            Console.WriteLine(studentList.Count());
+
+            //orderby
+            Console.WriteLine("******tolist\n");
+            students = _context.Students.OrderBy(x => x.StudentID).ToList();
+
+            foreach (var st in students)
+            {
+                Console.WriteLine(st.StudentID + " " + st.Name + " " + st.Surname);
+            }
+
+            //orderby desc
+            Console.WriteLine("******tolist\n");
+            students = _context.Students.OrderByDescending(x => x.StudentID).ToList();
+
+            foreach (var st in students)
+            {
+                Console.WriteLine(st.StudentID + " " + st.Name + " " + st.Surname);
+            }
 
 
         }
